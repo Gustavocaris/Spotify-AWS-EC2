dd43cb032eb0ebe78c8bef843aa04cbf39430859
Gustavo Caris <gustavocaris14052002@gmail.com>
2026-02-07 12:21:47 -0300

    feat: initialize Javascript file. new version, more simple

---
+++ Frontend/script.js
@@ -0,0 +1,5 @@
+// Reset básico
+document.body.style.margin = "0";
+document.body.style.fontFamily = "Helvetica, Arial, sans-serif";
+document.body.style.background = "#121212";
+document.body.style.color = "#ffffff";
