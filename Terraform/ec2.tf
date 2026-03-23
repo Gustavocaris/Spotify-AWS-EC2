ee7f52d0abccc1e7280ca5d41ebfdf81cf4b17ab
Gustavo Caris <gustavocaris14052002@gmail.com>
2026-03-23 15:24:24 -0300

    feat(): Adds ingress rule for port 443 (HTTPS) and a general egress rule to permit all outbound traffic from the security group

--- Terraform/ec2.tf
+++ Terraform/ec2.tf
@@ -28,3 +28,18 @@ resource "aws_vpc_security_group_ingress_rule" "allow_http" {
   ip_protocol       = "tcp"
   to_port           = 80
 }
+
+resource "aws_vpc_security_group_ingress_rule" "allow_https" {
+  security_group_id = aws_security_group.website_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_outbound" {
+  security_group_id = aws_security_group.website_sg.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  ip_protocol = -1
+}
